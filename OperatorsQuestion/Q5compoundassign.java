eb9bbee3c29a3f705024e9bae05b1614ec5b3459
sanjay-2420030014 <2420030014@klh.edu.in>
2025-08-02 08:28:37 +0530

    Add files via upload
    PROJECT -1 (OPERATORS QUESTIONS)

---
+++ OperatorsQuestion/Q5compoundassign.java
@@ -0,0 +1,13 @@
+package OperatorsQuestion;
+
+public class Q5compoundassign {
+//5. Demonstrate compound assignment operators on a number.
+	public static void main(String[] args) {
+		int x=5;
+		x+=10;
+		x*=2;
+		x/=2;
+		x%=2;
+		System.out.println(x);
+	}
+}
---
+++ OperatorsQuestion/Q6increment.java
@@ -0,0 +1,14 @@
+package OperatorsQuestion;
+
+public class Q6increment {
+	//Demonstrate increment and decrement operators on a variable.
+	public static void main(String[] args) {
+		int x=5;
+		System.out.println(x);
+		x++;
+		System.out.println("X++"+x);
+		x--;
+		System.out.println("X--"+x);
+	}
+
+}
